45d26d732a38e5a31d6e0449ace62fbde5ad8115
Ricardo A. Rubio <rickarubio@gmail.com>
2013-05-18 11:20:04 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Posty::Application.config.secret_token = '171b83c0583ae6a1cc124fffe16ce10e019be7afa2179247c3ca6c0245d99b6e2c5cf33ee83da84ee84fe26960d29eb94aa9e1576e168462c6bfd8c32fa722ee'
