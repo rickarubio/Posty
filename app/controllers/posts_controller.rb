aab6053751bfc91889b402e1377ff8b28af0d613
Chad Jemmett <chad.jemmett@gmail.com>
2013-05-31 21:19:32 -0600

    Can create new posty. Show posty. Show url. They expire after 120

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,30 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    if Time.now > @post.created_at + 120
+	    render text: "Sorry this posty has expired"
+    end
   end
 
-  def new
 
+  def new
+    @post = Post.new
+
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+    redirect_to @post
+   # else
+#	    render 'new'
+   # end
+    #render text: params[:post]
+   # @post = Post.new(params[:text])
+   # @post.save
+   # redirect_to @post
+   
   end
 
 end
